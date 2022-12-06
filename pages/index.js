be07c9d0435d2bf79ebcedd2c8e0a543620f79b6
vinicius fialho <vinicius.garcia@bitka.com.br>
2022-12-06 18:34:34 -0300

    fix
    fix

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default function Home({ allPostsData }) {
       </section>
       <Link href = "/api/hello"> Say Hello </Link>
       <Link href="/posts/first-post"> Go </Link>
+      <Link href = "/api/hello"> Say Hello Again </Link>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
